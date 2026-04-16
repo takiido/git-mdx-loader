1582148d90bf1e195a9cd756e62ea34d9f3a8754
takiido <takiid0@proton.me>
2026-04-15 21:12:24 -0500

    added gfm plugin for additional components

--- src/render-content.tsx
+++ src/render-content.tsx
@@ -1,10 +1,11 @@
 import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 import type { RenderContentProps } from "./types.js";
 
 export function RenderContent({ content, components, className }: RenderContentProps) {
   return (
     <div className={className}>
-      <ReactMarkdown components={components}>{content}</ReactMarkdown>
+      <ReactMarkdown remarkPlugins={[remarkGfm]} components={components}>{content}</ReactMarkdown>
     </div>
   );
 }
